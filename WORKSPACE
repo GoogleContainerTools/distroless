813d1ddef217f3871e4cb0a73da100aeddc638ee
dlorenc <lorenc.d@gmail.com>
2018-04-26 11:33:55 -0700

    Update rules_go. (#189)
    * Update rules_go.
    
    Signed-off-by: dlorenc <dlorenc@google.com>
    
    * Fix tests.
    
    * Buildifier

--- WORKSPACE
+++ WORKSPACE
@@ -3,7 +3,7 @@ workspace(name = "distroless")
 git_repository(
     name = "io_bazel_rules_go",
     remote = "https://github.com/bazelbuild/rules_go.git",
-    tag = "0.7.0",
+    tag = "0.11.0",
 )
 
 load("@io_bazel_rules_go//go:def.bzl", "go_rules_dependencies", "go_register_toolchains")
--- buildifier.sh
+++ buildifier.sh
@@ -14,12 +14,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-files=$(buildifier -mode=check $(find . -name 'BUILD*' -o -name 'WORKSPACE*' -o -name '*.bzl' -type f))
+files=$(buildifier -mode=check $(find . -name 'BUILD*' -o -name 'WORKSPACE*' -type f))
 if [ $? -ne 0 ]; then
   exit 1
 fi
 
 if [[ $files ]]; then
-  echo "Run 'buildifier -mode fix \$(find . -name 'BUILD*' -o -name 'WORKSPACE*' -o -name '*.bzl' -type f)' to fix formatting"
+  echo "Run 'buildifier -mode fix \$(find . -name 'BUILD*' -o -name 'WORKSPACE*' -type f)' to fix formatting"
   exit 1
 fi
