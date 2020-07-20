a1cc01898f9f04b522ad3d9219cb138cb936b613
Peter Mylemans <peter.mylemans@gmail.com>
2020-07-20 22:28:49 +0200

    Update locale/extract.sh to only include C.UTF-8
    Co-authored-by: Chanseok Oh <chanseok@google.com>

--- locale/extract.sh
+++ locale/extract.sh
@@ -7,7 +7,7 @@ OUT_TAR=$2
 
 ar -x "$DEB" data.tar.xz
 
-tar -xf data.tar.xz ./usr/lib/locale ./usr/share/doc/libc-bin/copyright
+tar -xf data.tar.xz ./usr/lib/locale/C.UTF-8 ./usr/share/doc/libc-bin/copyright
 
 rm data.tar.xz
 
