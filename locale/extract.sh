c5375196398a5aa618591be44d6512f28d8f6619
Peter Mylemans <peter.mylemans@corilus.be>
2020-07-20 23:19:08 +0200

    Clean up extract.sh and document gnu tar

--- locale/extract.sh
+++ locale/extract.sh
@@ -11,6 +11,8 @@ tar -xf data.tar.xz ./usr/lib/locale/C.UTF-8 ./usr/share/doc/libc-bin/copyright
 
 rm data.tar.xz
 
+# On Mac OS, you need to make sure that GNU Tar is used instead of the BSD tar that is shipped with MacOS.
+# To resolve this you can install gnu-tar via Homebrew and symlink it as /usr/local/bin/tar.
 tar -c --owner=0 --group=0 -f locale.tar ./usr
 
 mv locale.tar "$OUT_TAR"
