6eb42cf14d348d93ea23bb1a5dd3dd101139dc6c
Morlay <morlay.null@gmail.com>
2020-11-07 11:20:19 +0800

    Publish cc*:*-{s390x,ppc64le}

--- cloudbuild_docker.sh
+++ cloudbuild_docker.sh
@@ -34,8 +34,8 @@ for distro_suffix in "" -debian9 -debian10; do
   docker_manifest gcr.io/$PROJECT_ID/base${distro_suffix}:debug-nonroot "amd64 arm64 s390x ppc64le"
   docker_manifest gcr.io/$PROJECT_ID/base${distro_suffix}:debug "amd64 arm64 s390x ppc64le"
 
-  docker_manifest gcr.io/$PROJECT_ID/cc${distro_suffix}:latest "amd64 arm64"
-  docker_manifest gcr.io/$PROJECT_ID/cc${distro_suffix}:debug "amd64 arm64"
+  docker_manifest gcr.io/$PROJECT_ID/cc${distro_suffix}:latest "amd64 arm64 s390x ppc64le"
+  docker_manifest gcr.io/$PROJECT_ID/cc${distro_suffix}:debug "amd64 arm64 s390x ppc64le"
 done
 
 docker manifest create gcr.io/$PROJECT_ID/python3:nonroot \
