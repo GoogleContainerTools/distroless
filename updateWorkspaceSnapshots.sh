8ddaf02e067f45ea56c5c8bbb2e60dd4265417bc
Chanseok Oh <chanseok@google.com>
2020-11-16 12:23:01 -0500

    Add comment (#639)

--- updateWorkspaceSnapshots.sh
+++ updateWorkspaceSnapshots.sh
@@ -39,6 +39,11 @@ cat > checksums.bzl <<EOF
 # TO GENERATE THIS RUN: ./updateWorkspaceSnapshots.sh
 
 BASE_ARCHITECTURES = ["amd64", "arm64"]
+# Exceptions:
+# - s390x doesn't have libunwind8.
+#   https://github.com/GoogleContainerTools/distroless/pull/612#issue-500157699
+# - ppc64le doesn't have stretch security-channel.
+#   https://github.com/GoogleContainerTools/distroless/pull/637#issuecomment-728139611
 ARCHITECTURES = BASE_ARCHITECTURES + ["s390x", "ppc64le"]
 
 VERSIONS = [
