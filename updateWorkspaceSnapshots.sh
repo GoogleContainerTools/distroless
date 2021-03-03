9124185ae9061ccf5aa261084de5ccac8972f3e1
Chanseok Oh <chanseok@google.com>
2021-03-03 10:40:28 -0500

    Prevent concurrent runs

--- updateWorkspaceSnapshots.sh
+++ updateWorkspaceSnapshots.sh
@@ -131,16 +131,16 @@ EOF
 
 bazel clean
 bazel build --host_force_python=PY2 //package_manager:dpkg_parser.par
-bazel build --host_force_python=PY2 @package_bundle_amd64_debian9//file:packages.bzl
-bazel build --host_force_python=PY2 @package_bundle_amd64_debian10//file:packages.bzl
-bazel build --host_force_python=PY2 @package_bundle_arm_debian9//file:packages.bzl
-bazel build --host_force_python=PY2 @package_bundle_arm_debian10//file:packages.bzl
-bazel build --host_force_python=PY2 @package_bundle_arm64_debian9//file:packages.bzl
-bazel build --host_force_python=PY2 @package_bundle_arm64_debian10//file:packages.bzl
-bazel build --host_force_python=PY2 @package_bundle_s390x_debian9//file:packages.bzl
-bazel build --host_force_python=PY2 @package_bundle_s390x_debian10//file:packages.bzl
-bazel build --host_force_python=PY2 @package_bundle_ppc64le_debian9//file:packages.bzl
-bazel build --host_force_python=PY2 @package_bundle_ppc64le_debian10//file:packages.bzl
+bazel build --jobs=1 --host_force_python=PY2 @package_bundle_amd64_debian9//file:packages.bzl
+bazel build --jobs=1 --host_force_python=PY2 @package_bundle_amd64_debian10//file:packages.bzl
+bazel build --jobs=1 --host_force_python=PY2 @package_bundle_arm_debian9//file:packages.bzl
+bazel build --jobs=1 --host_force_python=PY2 @package_bundle_arm_debian10//file:packages.bzl
+bazel build --jobs=1 --host_force_python=PY2 @package_bundle_arm64_debian9//file:packages.bzl
+bazel build --jobs=1 --host_force_python=PY2 @package_bundle_arm64_debian10//file:packages.bzl
+bazel build --jobs=1 --host_force_python=PY2 @package_bundle_s390x_debian9//file:packages.bzl
+bazel build --jobs=1 --host_force_python=PY2 @package_bundle_s390x_debian10//file:packages.bzl
+bazel build --jobs=1 --host_force_python=PY2 @package_bundle_ppc64le_debian9//file:packages.bzl
+bazel build --jobs=1 --host_force_python=PY2 @package_bundle_ppc64le_debian10//file:packages.bzl
 
 # Check if any of the version lock files are updated
 
