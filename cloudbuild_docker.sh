55e6a86260541bc5d245b161de0f374c61495076
Appu Goundan <appu@google.com>
2022-03-03 10:43:33 -0500

    update python3:* manifest lists
    Signed-off-by: Appu Goundan <appu@google.com>

--- cloudbuild_docker.sh
+++ cloudbuild_docker.sh
@@ -44,10 +44,12 @@ done
 
 # python node and java are debian11 only
 
-docker_manifest gcr.io/$PROJECT_ID/python3-debian11:nonroot "amd64 arm64"
-docker_manifest gcr.io/$PROJECT_ID/python3-debian11:latest "amd64 arm64"
-docker_manifest gcr.io/$PROJECT_ID/python3-debian11:debug-nonroot "amd64 arm64"
-docker_manifest gcr.io/$PROJECT_ID/python3-debian11:debug "amd64 arm64"
+for distro_suffix in "" debian11; do
+  docker_manifest gcr.io/$PROJECT_ID/python3${distro_suffix}:nonroot "amd64 arm64"
+  docker_manifest gcr.io/$PROJECT_ID/python3${distro_suffix}:latest "amd64 arm64"
+  docker_manifest gcr.io/$PROJECT_ID/python3${distro_suffix}:debug-nonroot "amd64 arm64"
+  docker_manifest gcr.io/$PROJECT_ID/python3${distro_suffix}:debug "amd64 arm64"
+done
 
 for java_version in -base 11 17; do
   docker_manifest gcr.io/$PROJECT_ID/java${java_version}-debian11:latest "amd64 arm64"
