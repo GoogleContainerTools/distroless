49c89ed22f0eff6bf411e5b37b67a3357c52b44c
Appu Goundan <appu@google.com>
2022-07-28 14:01:42 -0700

    Actually fail if we don't succesfully update
    Signed-off-by: Appu Goundan <appu@google.com>

--- updateWorkspaceSnapshots.sh
+++ updateWorkspaceSnapshots.sh
@@ -8,4 +8,7 @@ set -o xtrace
 
 # the default config and output files are all in the current working
 # working directory
-for i in $(seq 5); do ./debian_package_manager/update && break || sleep 60; done
+for i in $(seq 5); do ./debian_package_manager/update && exit 0 || sleep 60; done
+
+# we didn't succesfully complete an update
+exit 1
