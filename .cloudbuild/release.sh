56d0667aecde748991627eed2ee092b4f1cffb83
thesayyn <thesayyn@gmail.com>
2023-02-22 22:27:57 +0300

    revert build_tar

--- .cloudbuild/release.sh
+++ .cloudbuild/release.sh
@@ -19,6 +19,6 @@ echo "common --google_default_credentials" >> ~/.bazelrc
 echo "common --announce_rc" >> ~/.bazelrc
 
 for i in $(seq 5); do 
-    bazel cquery 'kind(merge_providers, deps(kind(oci_image, ...)))' --config=release --output=label && break || sleep 20;
+    bazel cquery 'kind(merge_providers, deps(kind(oci_image, ...)))' --output=label --config=release && break || sleep 20;
 done
 bazel run :sign_and_push --config=release -- --key $KEY
---
+++ build_tar/README.md
@@ -0,0 +1,5 @@
+# build_tar
+
+Tool to build tar files from a list of inputs.
+
+All files here where copied over from [rules_pkg repo](https://github.com/bazelbuild/rules_pkg/tree/0.4.0/pkg).
